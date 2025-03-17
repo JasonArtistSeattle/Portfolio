9235be81aeffa4f8da2bb90059cf95f640e47d34
Kugusu <elias.lafornara@digitalskillsforall.org>
2025-03-17 11:27:48 -0700

    Fix images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,61 @@
 const content = [
 {
-    image: "assets/images/january.jpg",
+    image: "january.jpg",
     title: "Front",
     medium: "Acrylic on Canvas"
 },
 {
-    image: "assets/images/february.jpg",
+    image: "february.jpg",
     title: "Apples",
     medium: "Acrylic on Canvas"
 },
 {
-    image: "assets/images/march.jpg",
+    image: "march.jpg",
     title: "Morning Watch",
     medium: "Mixed Acrylic on Canvas"
 },
 {
-    image: "assets/images/april.jpg",
+    image: "april.jpg",
     title: "All in All",
     medium: "Mixed Media on Paper"
 },
 {
-    image: "assets/images/may.jpg",
+    image: "may.jpg",
     title: "May - Fallow",
     medium: "Watercolor"
 },
 {
-    image: "assets/images/june.jpg",
+    image: "june.jpg",
     title: "Summer Inlet",
     medium: "Acrylic on Canvas"
 },
 {
-    image: "assets/images/july.jpg",
+    image: "july.jpg",
     title: "Jump",
     medium: "Mixed Media on Canvas"
 },
 {
-    image: "assets/images/august.jpg",
+    image: "august.jpg",
     title: "Buttermilk Morning",
     medium: "Acrylic on Canvas"
 },
 {
-    image: "assets/images/september.jpg",
+    image: "september.jpg",
     title: "Morning Call",
     medium: "Watercolor"
 },
 {
-    image: "assets/images/october.jpg",
+    image: "october.jpg",
     title: "Dawn",
     medium: "Acrylic on Textured Paper"
 },
 {
-    image: "assets/images/november.jpg",
+    image: "november.jpg",
     title: "November Storm",
     medium: "Mixed Media on Canvas"
 },
 {
-    image: "assets/images/december.jpg",
+    image: "december.jpg",
     title: "First Snow",
     medium: "Watercolor"
 },
@@ -80,7 +80,7 @@ for (let dict of content) {
     newElement.appendChild(titleTag);
     newElement.appendChild(mediumTag);
 
-    imageTag.src = `images/${dict["image"]}`;
+    imageTag.src = `assets/images/${dict["image"]}`;
     titleTag.textContent = dict["title"];
     mediumTag.textContent = `Medium: ${dict["medium"]}`;
 }
